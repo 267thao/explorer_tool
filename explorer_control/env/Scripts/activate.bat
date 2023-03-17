624f452d97d4f4ab6a61130b152c0606467a881e
267thao <267thao@gmail.com>
2023-03-17 05:32:01 -0700

    setup

--- explorer_control/env/Scripts/activate.bat
+++ explorer_control/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\Documents_Storage\personal stuff\explorer_control\env
+set VIRTUAL_ENV=C:\Users\thao1803\Documents\GitHub\explorer_tool\explorer_control\env
 
 if not defined PROMPT set PROMPT=$P$G
 
